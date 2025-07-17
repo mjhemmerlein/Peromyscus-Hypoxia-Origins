f0329871361b91a5604baf191d97b80d0bbd927c
mjhemmerlein <megan.hemmerlein@colostate.edu>
2025-07-17 15:18:19 -0600

    LP BWME WGCNA

--- WGCNA_RScripts/LP_WGCNA_BWME.R
+++ WGCNA_RScripts/LP_WGCNA_BWME.R
@@ -1,5 +1,5 @@
 
-library(WGCNA)
+library(WGCNA, exclude = "GO.db")
 library(dplyr)
 library(edgeR)
 library(lme4)
@@ -8,15 +8,18 @@ library(readxl)
 library(ggplot2)
 
 # Read in sample info
-Traits = read_xlsx("RNA_Seq_RawData/MetaData_EP.xlsx")
+Traits = read_xlsx("RNA_Seq_RawData/MetaData_LP.xlsx")
 Traits = as.data.frame(Traits)
 Traits = Traits %>%
   filter(Strain != "LL")
+Traits = Traits %>%
+  select(-c("Sample_ID_JZ", "JZ_Lane"))
+Traits = Traits[-c(80,81),]
 
-rownames(Traits) = Traits$Sample_ID
+rownames(Traits) = Traits$Sample_ID_LZ
 
-Traits$Sample_ID = as.factor(Traits$Sample_ID)
-Traits$Lane = as.factor(Traits$Lane)
+Traits$Sample_ID_LZ = as.factor(Traits$Sample_ID_LZ)
+Traits$LZ_Lane = as.factor(Traits$LZ_Lane)
 Traits$TubeID = as.factor(Traits$TubeID)
 Traits$Mom = as.factor(Traits$Mom)
 Traits$Strain = as.factor(Traits$Strain)
@@ -25,30 +28,32 @@ Traits$Sex = as.factor(Traits$Sex)
 Traits$Group = as.factor(Traits$Group)
 
 Traits = Traits %>%
-  select(c(-Order, -Set, -Sex_Group, -`Seq Read FWD`, -`Seq Read REV`))
+  select(c(-Sex_Group, -JZ_Seq_Name))
 
-TraitsBW_EP = Traits %>%
+Traits = Traits[-which(rownames(Traits) == "LZ089"),]
+
+TraitsBW_LP = Traits %>%
   filter(Strain == "BW")
 
-TraitsME_EP = Traits %>%
+TraitsME_LP = Traits %>%
   filter(Strain == "ME")
 
 
-
 # Read in Files + QC
-EPcounts = read_xlsx("RNA_Seq_RawData/EP_Pman_ExtMMFrac_readcounts_Exon.xlsx")
-EPcounts = as.data.frame(EPcounts)
-EPcounts = `row.names<-`(EPcounts, EPcounts$Geneid)
-EPcounts <- EPcounts[,-c(1:6)]
+LPcounts = read_xlsx("RNA_Seq_RawData/LP_Pman_ExtMMFrac_readcounts_Exon.xlsx")
+LPcounts = as.data.frame(LPcounts)
+LPcounts = `row.names<-`(LPcounts, LPcounts$Geneid)
+LPcounts = LPcounts[,-c(1:6)]
+LPcounts = subset(LPcounts, select = -c(RNA201216ZC_LZ089_S16_L001_fastp_pman_Halign_liberal.bam))
 
 
 # Check read table vs sample info
 Check = Traits$Seq_Name
-colnames(EPcounts) == Check
+colnames(LPcounts) == Check
 
-colnames(EPcounts) = rownames(Traits)
+colnames(LPcounts) = rownames(Traits)
 
-dPman_0 <- DGEList(EPcounts)
+dPman_0 <- DGEList(LPcounts)
 dPman_0 <- calcNormFactors(dPman_0)
 dim(dPman_0)
 
@@ -59,31 +64,31 @@ dim(dPman)
 rownames(Traits) == colnames(dPman)
 
 # WGCNA Analysis
-ExprData_EP = cpm(dPman, log=TRUE, prior.count=2, normalized.lib.sizes=TRUE) #cpm normalized and log transformed expression dataset
+ExprData_LP = cpm(dPman, log=TRUE, prior.count=2, normalized.lib.sizes=TRUE) #cpm normalized and log transformed expression dataset
 
 # Filter ME and BW
-ExprData_MEEP = ExprData_EP[,colnames(ExprData_EP) %in% rownames(TraitsME_EP)]
-ExprData_BWEP = ExprData_EP[,colnames(ExprData_EP) %in% rownames(TraitsBW_EP)]
+ExprData_MELP = ExprData_LP[,colnames(ExprData_LP) %in% rownames(TraitsME_LP)]
+ExprData_BWLP = ExprData_LP[,colnames(ExprData_LP) %in% rownames(TraitsBW_LP)]
 
 # Transpose expression data for further analysis
 # ME
-ExprData_MEEP = as.data.frame(t(ExprData_MEEP))
-ExprData_MEEP$treatment = TraitsME_EP$Strain
-ExprData_MEEP = ExprData_MEEP %>% select(-treatment)
+ExprData_MELP = as.data.frame(t(ExprData_MELP))
+ExprData_MELP$treatment = TraitsME_LP$Strain
+ExprData_MELP = ExprData_MELP %>% select(-treatment)
 
 # BW
-ExprData_BWEP = as.data.frame(t(ExprData_BWEP))
-ExprData_BWEP$treatment = TraitsBW_EP$Strain
-ExprData_BWEP = ExprData_BWEP %>% select(-treatment)
+ExprData_BWLP = as.data.frame(t(ExprData_BWLP))
+ExprData_BWLP$treatment = TraitsBW_LP$Strain
+ExprData_BWLP = ExprData_BWLP %>% select(-treatment)
 
-match(rownames(TraitsME_EP), rownames(ExprData_MEEP))# should be in order if datasets are matched
-match(rownames(TraitsBW_EP), rownames(ExprData_BWEP))
+match(rownames(TraitsME_LP), rownames(ExprData_MELP))# should be in order if datasets are matched
+match(rownames(TraitsBW_LP), rownames(ExprData_BWLP))
 
 # WGCNA
 
 # Lowlanders, i.e., BW ####
 # Cluster the samples to check for outliers
-sampleTreeBW = hclust(dist(ExprData_BWEP), method = "average")
+sampleTreeBW = hclust(dist(ExprData_BWLP), method = "average")
 # Plot the sample trees
 plot(sampleTreeBW, main = "Sample clustering to detect outliers", sub="", xlab="", cex.lab = 1.5,
      cex.axis = 1.5, cex.main = 2) 
@@ -91,7 +96,7 @@ plot(sampleTreeBW, main = "Sample clustering to detect outliers", sub="", xlab="
 ## Set power threshold
 # Choose a set of soft-thresholding powers
 powers = c(c(1:10), seq(from = 12, to=20, by=2))
-sftBW = pickSoftThreshold(ExprData_BWEP, powerVector = powers, verbose = 5, 
+sftBW = pickSoftThreshold(ExprData_BWLP, powerVector = powers, verbose = 5, 
                           networkType = "signed hybrid") 
 # Plot the results:
 #pdf('wgcna/rv_beta_plot.pdf', h=4, w=7)
@@ -119,10 +124,11 @@ plot(sftBW$fitIndices[,1],
 text(sftBW$fitIndices[,1], sftBW$fitIndices[,5], labels=powers, cex=cex1,col="red")
 
 
-
 # Call the network topology analysis function
-NetBWEP = blockwiseModules(ExprData_BWEP, 
-                           power = 20, 
+
+# POWER = 4
+NetBWLP = blockwiseModules(ExprData_BWLP, 
+                           power = 7, 
                            maxBlockSize = 14000,
                            TOMType = "signed", networkType = "signed hybrid",
                            minModuleSize = 30,
@@ -131,23 +137,23 @@ NetBWEP = blockwiseModules(ExprData_BWEP,
                            numericLabels = TRUE, 
                            pamRespectsDendro = FALSE,
                            saveTOMs = TRUE,
-                           saveTOMFileBase = "EP_WGCNA_Output/EPBWExprTOM",
+                           saveTOMFileBase = "LP_WGCNA_Output/LPBWExprTOM",
                            verbose = 3)
 
-load("EP_WGCNA_Output/EPBWExprTOM-block.1.RData"); load("EP_WGCNA_Output/EPBWExprTOM-block.2.RData")
-table(NetBWEP$colors)
-moduleLabelsBWEP = NetBWEP$colors
-moduleColorsBWEP = labels2colors(NetBWEP$colors)
+load("LP_WGCNA_Output/LPBWExprTOM-block.1.RData"); load("LP_WGCNA_Output/LPBWExprTOM-block.2.RData")
+table(NetBWLP$colors)
+moduleLabelsBWLP = NetBWLP$colors
+moduleColorsBWLP = labels2colors(NetBWLP$colors)
 
-MEsBW_EP = NetBWEP$MEs # These are module eigengenes:first principal component of the expression matrix, 
+MEsBW_LP = NetBWLP$MEs # These are module eigengenes:first principal component of the expression matrix, 
 # i.e., weighted average expression profile of a sample in a module
-geneTreeBWEP = NetBWEP$dendrograms[[1]]
-table(moduleColorsBWEP)
-dim(table(moduleColorsBWEP))
+geneTreeBWLP = NetBWLP$dendrograms[[1]]
+table(moduleColorsBWLP)
+dim(table(moduleColorsBWLP))
 
 plotDendroAndColors(
-  NetBWEP$dendrograms[[1]],
-  moduleColorsBWEP[NetBWEP$blockGenes[[1]]],
+  NetBWLP$dendrograms[[1]],
+  moduleColorsBWLP[NetBWLP$blockGenes[[1]]],
   "Module colors",
   dendroLabels = FALSE,
   hang = 0.03, 
@@ -155,61 +161,61 @@ plotDendroAndColors(
   guideHang = 0.05)
 
 # Recalculate MEs with color labels
-MEs0 = moduleEigengenes(ExprData_BWEP, moduleColorsBWEP)$eigengenes
-MEsBW_EP = orderMEs(MEs0) #the rownames of this dataset are equal to Expr
+MEs0 = moduleEigengenes(ExprData_BWLP, moduleColorsBWLP)$eigengenes
+MEsBW_LP = orderMEs(MEs0) #the rownames of this dataset are equal to Expr
 
-# write.csv(MEsBW_EP, "EP_WGCNA_Output/EP_BW_moduleEigengenes.csv")
-# save(NetBWEP, MEsBW_EP, moduleLabelsBWEP, moduleColorsBWEP, geneTreeBWEP, file = "EP_WGCNA_Output/BWEP_network.RData")
+write.csv(MEsBW_LP, "LP_WGCNA_Output/LP_BW_moduleEigengenes.csv")
+save(NetBWLP, MEsBW_LP, moduleLabelsBWLP, moduleColorsBWLP, geneTreeBWLP, file = "LP_WGCNA_Output/BWLP_network.RData")
 
-load("EP_WGCNA_Output/BWEP_network.RData")
+load("LP_WGCNA_Output/BWLP_network.RData")
 
 
 ## BW Trait Module Associations and Correlations ####
-ModuleME_Info_BWEP = read.csv("EP_WGCNA_Output/EP_BW_moduleEigengenes.csv")
-rownames(ModuleME_Info_BWEP) = ModuleME_Info_BWEP$X
-ModuleME_Info_BWEP = ModuleME_Info_BWEP %>% dplyr::select(-X)
-ModuleME_Info_BWEP = ModuleME_Info_BWEP %>% dplyr::select(-MEgrey) #remove grey module
+ModuleME_Info_BWLP = read.csv("LP_WGCNA_Output/LP_BW_moduleEigengenes.csv")
+rownames(ModuleME_Info_BWLP) = ModuleME_Info_BWLP$X
+ModuleME_Info_BWLP = ModuleME_Info_BWLP %>% dplyr::select(-X)
+ModuleME_Info_BWLP = ModuleME_Info_BWLP %>% dplyr::select(-MEgrey) #remove grey module
 
 # Correlate modules to hypoxia OVERALL w/ mom as random effect
-Strain = TraitsBW_EP$Strain
-O2 = TraitsBW_EP$O2
-FetalMass = TraitsBW_EP$Fetus
-Sex = TraitsBW_EP$Sex
-# summaryBWEP = data.frame()
-all_modules = colnames(ModuleME_Info_BWEP)
+Strain = TraitsBW_LP$Strain
+O2 = TraitsBW_LP$O2
+FetalMass = TraitsBW_LP$Fetus
+Sex = TraitsBW_LP$Sex
+# summaryBWLP = data.frame()
+all_modules = colnames(ModuleME_Info_BWLP)
 
-summaryBWEP02 = data.frame()
+summaryBWLP02 = data.frame()
 
 for (p in all_modules) {
   
-  test_data = ModuleME_Info_BWEP[, p]
-  test_model = lmer(test_data ~ O2 + (1|(TraitsBW_EP$Mom)))
+  test_data = ModuleME_Info_BWLP[, p]
+  test_model = lmer(test_data ~ O2 + (1|(TraitsBW_LP$Mom)))
   output = anova(test_model)
   output_line <- data.frame(
     Module = p,
     F.O2 = output$`F value`,
     Pvalue.O2 = output$`Pr(>F)`
   )
-  summaryBWEP02 = rbind(summaryBWEP02, output_line)
+  summaryBWLP02 = rbind(summaryBWLP02, output_line)
 }
 
-summaryBWEP02$Pvalue.O2corr <- p.adjust(summaryBWEP02$Pvalue.O2, method = 'BH')
+summaryBWLP02$Pvalue.O2corr <- p.adjust(summaryBWLP02$Pvalue.O2, method = 'BH')
 
-# write.csv(summaryBWEP02, "EP_WGCNA_Output/EP_BW_Module_Trait_ModelSummary.csv")
+write.csv(summaryBWLP02, "LP_WGCNA_Output/LP_BW_Module_Trait_ModelSummary.csv")
 
 # Create a dataset containing all gene-specific information
-genes=names(ExprData_BWEP)
-geneInfoBWEP = data.frame(Gene = genes,
-                          moduleColor = moduleColorsBWEP)
+genes=names(ExprData_BWLP)
+geneInfoBWLP = data.frame(Gene = genes,
+                          moduleColor = moduleColorsBWLP)
 
-# write.csv(geneInfoBWEP, "EP_WGCNA_Output/EP_BW_Modules.csv")
+write.csv(geneInfoBWLP, "LP_WGCNA_Output/LP_BW_Modules.csv")
 
 ## Plot eigenes against hypoxia/normoxia in BW animals ####
-ModuleME_Info_BWEP$hypoxia = TraitsBW_EP$O2
+ModuleME_Info_BWLP$hypoxia = TraitsBW_LP$O2
 
 #brown
-ggplot(data = ModuleME_Info_BWEP, aes(x = ModuleME_Info_BWEP$hypoxia, y = ModuleME_Info_BWEP$MEbrown)) + 
-  geom_boxplot(aes(fill = ModuleME_Info_BWEP$hypoxia), outlier.shape = NA) + theme_classic() + 
+ggplot(data = ModuleME_Info_BWLP, aes(x = ModuleME_Info_BWLP$hypoxia, y = ModuleME_Info_BWLP$MEbrown)) + 
+  geom_boxplot(aes(fill = ModuleME_Info_BWLP$hypoxia), outlier.shape = NA) + theme_classic() + 
   geom_jitter(width = 0.25) + 
   scale_fill_manual(values = c("red", "blue")) + 
   guides(fill = guide_legend(title = "Oxygen")) + 
@@ -220,15 +226,15 @@ ggplot(data = ModuleME_Info_BWEP, aes(x = ModuleME_Info_BWEP$hypoxia, y = Module
 ## Highlanders WGCNA network build, i.e., ME ####
 
 #cluster the samples to check for outliers
-sampleTreeMEEP = hclust(dist(ExprData_MEEP), method = "average")
+sampleTreeMELP = hclust(dist(ExprData_MELP), method = "average")
 # Plot the sample trees
-plot(sampleTreeMEEP, main = "Sample clustering to detect outliers", sub="", xlab="", cex.lab = 1.5,
+plot(sampleTreeMELP, main = "Sample clustering to detect outliers", sub="", xlab="", cex.lab = 1.5,
      cex.axis = 1.5, cex.main = 2) 
 
 ## Set power threshold
 # Choose a set of soft-thresholding powers
 powers = c(c(1:10), seq(from = 12, to=20, by=2))
-sftME = pickSoftThreshold(ExprData_MEEP, powerVector = powers, verbose = 5, 
+sftME = pickSoftThreshold(ExprData_MELP, powerVector = powers, verbose = 5, 
                           networkType = "signed hybrid") 
 # Plot the results:
 #pdf('wgcna/rv_beta_plot.pdf', h=4, w=7)
@@ -252,8 +258,10 @@ plot(sftME$fitIndices[,1], sftME$fitIndices[,5],
 text(sftME$fitIndices[,1], sftME$fitIndices[,5], labels=powers, cex=cex1,col="red")
 
 # Call the network topology analysis function
-NetMEEP = blockwiseModules(ExprData_MEEP, 
-                           power = 20, 
+
+# POWER = 4
+NetMELP = blockwiseModules(ExprData_MELP, 
+                           power = 7, 
                            maxBlockSize = 14000,
                            TOMType = "signed", 
                            networkType = "signed hybrid",
@@ -263,23 +271,23 @@ NetMEEP = blockwiseModules(ExprData_MEEP,
                            numericLabels = TRUE, 
                            pamRespectsDendro = FALSE,
                            saveTOMs = TRUE,
-                           saveTOMFileBase = "EP_WGCNA_Output/EPMEExprTOM",
+                           saveTOMFileBase = "LP_WGCNA_Output/LPMEExprTOM",
                            verbose = 3)
 
-load("EP_WGCNA_Output/EPMEExprTOM-block.1.RData"); load("EP_WGCNA_Output/EPMEExprTOM-block.2.RData")
-table(NetMEEP$colors)
-moduleLabelsMEEP = NetMEEP$colors
-moduleColorsMEEP = labels2colors(NetMEEP$colors)
-MEsMEEP = NetMEEP$MEs # These are module eigengenes:first principal component of the expression matrix, 
+load("LP_WGCNA_Output/LPMEExprTOM-block.1.RData"); load("LP_WGCNA_Output/LPMEExprTOM-block.2.RData")
+table(NetMELP$colors)
+moduleLabelsMELP = NetMELP$colors
+moduleColorsMELP = labels2colors(NetMELP$colors)
+MEsMELP = NetMELP$MEs # These are module eigengenes:first principal component of the expression matrix, 
 # i.e., weighted average expression profile of a sample in a module
 
-geneTreeMEEP = NetMEEP$dendrograms[[1]]
-table(moduleColorsMEEP)
-dim(table(moduleColorsMEEP))
+geneTreeMELP = NetMELP$dendrograms[[1]]
+table(moduleColorsMELP)
+dim(table(moduleColorsMELP))
 
 plotDendroAndColors(
-  NetMEEP$dendrograms[[1]],
-  moduleColorsMEEP[NetMEEP$blockGenes[[1]]],
+  NetMELP$dendrograms[[1]],
+  moduleColorsMELP[NetMELP$blockGenes[[1]]],
   "Module colors",
   dendroLabels = FALSE,
   hang = 0.03, 
@@ -287,54 +295,54 @@ plotDendroAndColors(
   guideHang = 0.05)
 
 # Recalculate MEs with color labels
-MEs0ME = moduleEigengenes(ExprData_MEEP, moduleColorsMEEP)$eigengenes
-MEsMEEP = orderMEs(MEs0ME) #the rownames of this dataset are equal to Expr
+MEs0ME = moduleEigengenes(ExprData_MELP, moduleColorsMELP)$eigengenes
+MEsMELP = orderMEs(MEs0ME) #the rownames of this dataset are equal to Expr
 
 #Add metadata to this dataframe
-MEsMEEP$hypoxia = TraitsME_EP$O2
-MEsMEEP$fetalMass = TraitsME_EP$Fetus
+MEsMELP$hypoxia = TraitsME_LP$O2
+MEsMELP$fetalMass = TraitsME_LP$Fetus
 
-# write.csv(MEsMEEP, "EP_WGCNA_Output/EP_ME_moduleEigengenes.csv")
+write.csv(MEsMELP, "LP_WGCNA_Output/LP_ME_moduleEigengenes.csv")
 
-# save(NetMEEP, MEsMEEP, moduleLabelsMEEP, moduleColorsMEEP, geneTreeMEEP, file = "EP_WGCNA_Output/MEEP_network.RData")
-load("EP_WGCNA_Output/MEEP_network.RData")
+save(NetMELP, MEsMELP, moduleLabelsMELP, moduleColorsMELP, geneTreeMELP, file = "LP_WGCNA_Output/MELP_network.RData")
+load("LP_WGCNA_Output/MELP_network.RData")
 
 
 ## ME Trait Module Associations and Correlations ####
-ModuleME_Info_MEEP = read.csv("EP_WGCNA_Output/EP_ME_moduleEigengenes.csv")
-rownames(ModuleME_Info_MEEP) = ModuleME_Info_MEEP$X
-ModuleME_Info_MEEP = ModuleME_Info_MEEP %>% dplyr::select(-X)
-ModuleME_Info_MEEP = ModuleME_Info_MEEP %>% dplyr::select(-MEgrey) #remove grey module
-ModuleME_Info_MEEP = ModuleME_Info_MEEP %>% select(-hypoxia)
+ModuleME_Info_MELP = read.csv("LP_WGCNA_Output/LP_ME_moduleEigengenes.csv")
+rownames(ModuleME_Info_MELP) = ModuleME_Info_MELP$X
+ModuleME_Info_MELP = ModuleME_Info_MELP %>% dplyr::select(-X)
+ModuleME_Info_MELP = ModuleME_Info_MELP %>% dplyr::select(-MEgrey) #remove grey module
+ModuleME_Info_MELP = ModuleME_Info_MELP %>% select(-hypoxia)
 
 # Correlate modules to hypoxia OVERALL w/ mom as random effect
-Strain = TraitsME_EP$Strain
-O2 = TraitsME_EP$O2
-FetalMass = TraitsME_EP$Fetus
-Sex = TraitsME_EP$Sex
-# summaryMEEP = data.frame()
-all_modules = colnames(ModuleME_Info_MEEP)
+Strain = TraitsME_LP$Strain
+O2 = TraitsME_LP$O2
+FetalMass = TraitsME_LP$Fetus
+Sex = TraitsME_LP$Sex
+# summaryMELP = data.frame()
+all_modules = colnames(ModuleME_Info_MELP)
 
-summaryMEEP02 = data.frame()
+summaryMELP02 = data.frame()
 
 for (p in all_modules) {
   
-  test_data = ModuleME_Info_MEEP[, p]
-  test_model = lmer(test_data ~ O2 + (1|(TraitsME_EP$Mom)))
+  test_data = ModuleME_Info_MELP[, p]
+  test_model = lmer(test_data ~ O2 + (1|(TraitsME_LP$Mom)))
   output = anova(test_model)
   output_line <- data.frame(
     Module = p,
     F.O2 = output$`F value`,
     Pvalue.O2 = output$`Pr(>F)`
   )
-  summaryMEEP02 = rbind(summaryMEEP02, output_line)
+  summaryMELP02 = rbind(summaryMELP02, output_line)
 }
 
-summaryMEEP02$Pvalue.O2corr <- p.adjust(summaryMEEP02$Pvalue.O2, method = 'BH')
+summaryMELP02$Pvalue.O2corr <- p.adjust(summaryMELP02$Pvalue.O2, method = 'BH')
 
-# write.csv(summaryMEEP02, "EP_WGCNA_Output/EP_ME_Module_Trait_ModelSummary.csv")
+write.csv(summaryMELP02, "LP_WGCNA_Output/LP_ME_Module_Trait_ModelSummary.csv")
 
 # Create a dataset containing all gene-specific information
-genes=names(ExprData_MEEP)
-geneInfoMEEP = data.frame(Gene = genes,
-                          moduleColor = moduleColorsMEEP)
+genes=names(ExprData_MELP)
+geneInfoMELP = data.frame(Gene = genes,
+                          moduleColor = moduleColorsMELP)
